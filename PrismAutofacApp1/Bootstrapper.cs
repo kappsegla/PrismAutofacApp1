4f4fc9e95bd7984a1dc90c6d2f2eda33fb612639
Martin Blomberg <martin.blomberg@outlook.com>
2017-11-01 18:08:39 +0100

    Add project files.

---
+++ PrismAutofacApp1/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Prism.Autofac;
+using PrismAutofacApp1.Views;
+using System.Windows;
+
+namespace PrismAutofacApp1
+{
+    class Bootstrapper : AutofacBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ PrismAutofacApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace PrismAutofacApp1.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Autofac Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
